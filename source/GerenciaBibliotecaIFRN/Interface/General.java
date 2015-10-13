e379ade1b38d07f66c42c03ef33754ac633fa17f
DanielsonSilva <danielson_fx@yahoo.com.br>
2015-10-13 18:19:31 -0300

    Início da Interface
    Criação de pacotes e classes e construção da interface principal do
    programa junto com arquivos para internacionalização do programa para
    Inglês e Francês.

---
+++ source/GerenciaBibliotecaIFRN/Interface/General.java
@@ -0,0 +1,125 @@
+package Interface;
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.Math.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Main window of the program
+ * 
+ * @author Danielson Silva (danielson_fx@yahoo.com.br)
+ * @version 1.0
+ */
+public class General extends JFrame
+{
+    // Numbers variables
+    private int width;
+    private int height;
+    private double sizeRate;
+    // Components variables
+    private JButton btnRegisterUse;
+    private JButton btnInsertUser;
+    private JButton btnRemoveUse;
+    private JButton btnReport;
+    /**
+     * Constructor for the General Class
+     * 
+     * @param messages Variable for intercionalization of the program
+     */
+    public General(ResourceBundle messages)
+    {
+        // inicialize variables
+        String title = messages.getString("title");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        sizeRate = 0.7;
+        width  = (int) (Math.round(dim.width*sizeRate));
+        height = (int) (Math.round(dim.height*sizeRate));
+
+        // Setting up the icon
+        URL url = this.getClass().getResource("/./utils/icon.png");    
+        Image icon = Toolkit.getDefaultToolkit().getImage(url);    
+        this.setIconImage(icon);        
+        
+        // Close operation
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Inicialize the window with 80% of the screen resolution
+        this.setSize(width, height);
+        // Title of the window
+        this.setTitle(title);
+        // Border Layout for the components
+        this.setLayout(new GridBagLayout());
+        // Inicialize the window in the center of the monitor
+        this.setLocationRelativeTo(null);  
+        // Add the menu bar
+        addMenuBarToPane(this,messages);
+        // Add the components of the window
+        addComponentsToPane(this.getContentPane(),messages);
+        // Popup the main window
+        this.setVisible(true);
+    }
+
+    /**
+     * Add the components to the container of this Class
+     * 
+     * @param pane Container that will receive the components
+     * @param messages Variable for internationalization
+     */
+    private void addComponentsToPane(Container pane, ResourceBundle messages)
+    {
+        //Setting the names of the buttons
+        String strRegisterUse = messages.getString("btnRegisterUse");
+        String strInsertUser = messages.getString("btnInsertUser");
+        String strRemoveUse = messages.getString("btnRemoveUse");
+        String strReport = messages.getString("btnReport");
+        
+        //Setting up the pane
+        pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        GridBagConstraints gbc = new GridBagConstraints();
+        btnRegisterUse = new JButton(strRegisterUse);
+        btnInsertUser = new JButton(strInsertUser);
+        btnRemoveUse = new JButton(strRemoveUse);
+        btnReport = new JButton(strReport);
+        
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        pane.add(btnRegisterUse, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        pane.add(btnInsertUser, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        pane.add(btnRemoveUse, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        pane.add(btnReport, gbc);
+    }
+    
+    /**
+     * Add a menu bar to the program
+     * 
+     * @param pane Container that will receive the components
+     * @param messages Variable for internationalization
+     */    
+    private void addMenuBarToPane(JFrame frame, ResourceBundle messages)
+    {
+        // Setting up the menu bar
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        // Setting the menu categories
+        JMenu fileMenu = new JMenu(messages.getString("MenuFile"));
+        JMenu helpMenu = new JMenu(messages.getString("MenuHelp"));
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+        // Setting the menu items
+        JMenuItem exitAction = new JMenuItem(messages.getString("MenuItemExit"));
+        JMenuItem aboutAction = new JMenuItem(messages.getString("MenuItemAbout"));
+        // Setting up the adding
+        fileMenu.add(exitAction);
+        helpMenu.add(aboutAction);
+    }
+}
---
+++ source/GerenciaBibliotecaIFRN/Interface/Main.java
@@ -0,0 +1,52 @@
+package Interface;
+
+import java.util.*;
+
+/**
+ * Beginning of the program
+ * 
+ * @author Danielson Silva (danielson_fx@yahoo.com.br)
+ * @version 1.0
+ */
+public class Main
+{
+    // Variable for intercionalization
+    private ResourceBundle messages;
+    /**
+     * Constructor for the Main Class
+     */
+    public Main(ResourceBundle message)
+    {
+        // Inicialize the variable responsible for internationalization
+        this.messages = message;
+        General general = new General(messages);
+    }
+
+    /**
+     * Executing the start of the program
+     * 
+     * @param  args   arguments for internationalization
+     * @opt br BR Make the program in Brazilian Portuguese
+     * @opt en US Make the program in English
+     * @opt fr FR Make the program in French
+     */
+    public static void main(String args[])
+    {
+        String language;
+        String country;
+
+        if (args.length != 2) {
+            language = new String("br");
+            country = new String("BR");
+        } else {
+            language = new String(args[0]);
+            country = new String(args[1]);
+        }
+
+        Locale currentLocale;
+        currentLocale = new Locale(language, country);
+        ResourceBundle international = ResourceBundle.getBundle("utils.MessagesBundle", currentLocale);
+        
+        Main main = new Main(international);
+    }
+}
