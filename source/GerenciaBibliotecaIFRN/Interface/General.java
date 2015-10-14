f1aff7d61af503f7175e8a75937e3b3e546c6f7b
DanielsonSilva <danielson_fx@yahoo.com.br>
2015-10-14 18:56:06 -0300

    Atualização na Janela principal
    Inserção do JTabbedPane e alguns testes além de uma demonstração do uso
    do TabbedPane

--- source/GerenciaBibliotecaIFRN/Interface/General.java
+++ source/GerenciaBibliotecaIFRN/Interface/General.java
@@ -1,6 +1,7 @@
 package Interface;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.lang.Math.*;
 import java.net.*;
@@ -55,6 +56,8 @@ public class General extends JFrame
         this.setLocationRelativeTo(null);  
         // Add the menu bar
         addMenuBarToPane(this,messages);
+        // Add the tabbed pane
+        addTabbedPane(this, messages);
         // Add the components of the window
         addComponentsToPane(this.getContentPane(),messages);
         // Popup the main window
@@ -82,23 +85,20 @@ public class General extends JFrame
         btnInsertUser = new JButton(strInsertUser);
         btnRemoveUse = new JButton(strRemoveUse);
         btnReport = new JButton(strReport);
-        
+        // Adding the buttons
         gbc.insets = new Insets(5,5,5,5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.gridx = 0;
+        gbc.gridx = 1;
         gbc.gridy = 0;
         pane.add(btnRegisterUse, gbc);
-        gbc.gridx = 0;
         gbc.gridy = 1;
         pane.add(btnInsertUser, gbc);
-        gbc.gridx = 0;
         gbc.gridy = 2;
         pane.add(btnRemoveUse, gbc);
-        gbc.gridx = 0;
         gbc.gridy = 3;
         pane.add(btnReport, gbc);
     }
-    
     /**
      * Add a menu bar to the program
      * 
@@ -121,5 +121,67 @@ public class General extends JFrame
         // Setting up the adding
         fileMenu.add(exitAction);
         helpMenu.add(aboutAction);
+    }
+    /**
+     * Add the tabbed pane to the window
+     * 
+     * @param pane Container that will receive the components
+     * @param messages Variable for internationalization
+     */    
+    private void addTabbedPane(Container pane, ResourceBundle messages)
+    {
+        // Create a tabbed pane
+        JTabbedPane tabbedPane = new JTabbedPane();
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.gridheight = 4;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        
+        JComponent panel1 = makeTextPanel("Panel #1");
+        ImageIcon icon = createImageIcon("images/middle.gif");
+        tabbedPane.addTab("Tab 1", icon, new JTable(),"Does nothing");
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+        JComponent panel2 = makeTextPanel("Panel #2");
+        tabbedPane.addTab("Tab 2", icon, panel2,"Does twice as much nothing");
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+
+        JComponent panel3 = makeTextPanel("Panel #3");
+        tabbedPane.addTab("Tab 3", icon, panel3,"Still does nothing");
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+
+        JComponent panel4 = makeTextPanel("Panel #4 (has a preferred size of 410 x 50).");
+        panel4.setPreferredSize(new Dimension(410, 50));
+        tabbedPane.addTab("Tab 4", icon, panel4,"Does nothing at all");
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+        
+        pane.add(tabbedPane, gbc);
+    }
+    /**
+     * Add a TextPanel for a TabbedPane
+     * 
+     * @param text String that will appear in the panel
+     */  
+    private JComponent makeTextPanel(String text) 
+    {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }    
+    
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = TabbedPaneDemo.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
     }
 }
